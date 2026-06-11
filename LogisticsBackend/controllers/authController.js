c34d951d51b24fc6b92d69f670d90ddf87fdf6a4
Kelechi <ekelechi413@gmail.com>
2026-06-11 20:36:25 +0100

    done

--- LogisticsBackend/controllers/authController.js
+++ LogisticsBackend/controllers/authController.js
@@ -38,49 +38,38 @@ const register = async (req, res) => {
 
 //LOGIN A USER
 const login = async (req, res) => {
-    const { email, password } = req.body
-    if (!email || !password) {
-        return res.status(400).json({
-            message: "Please fill in the fields"
+   const { email, password } = req.body;
+    try {
+        const result = await pool.query("SELECT * FROM users  WHERE email = $1", [email]);
+        const user = result.rows[0];
+        if (!user) return res.status(400).json({
+            error: "user not found"
+        });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({
+            error: "Invalid credentials"
+        });
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                fullname: user.fullname,
+                email: user.email,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        )
+        res.json({ token, user });
+        console.log("DB USER:", user);
+        console.log("ROLE FROM DB:", user.role);
+    } catch (error) {
+        console.error("LOGIN ERROR", error.message);
+        res.status(500).json({
+            error: "Server error"
         });
     };
-    try {
-        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email.toLowerCase().trim()]);
-        if (user.rows.length === 0) {
-            return res.status(400).json({
-                message: "User does not exist"
-            });    
-        }
-
-        const isMatch = await bcrypt.compare(password, user.rows[0].password);
-        if (!isMatch){
-            return res.status(400).json({
-                message: "Invalid credentials"
-            });
-        }
-
-        const token = jwt.sign({
-            id: user.rows[0].id,
-            email: user.rows[0].email,
-            role: user.rows[0].role
-        },
-        process.env.JWT_SECRET,
-        {expiresIn: '7d'}
-    );
-        return res.status(200).json({
-            message: "Login successful",
-            token: token,
-            user: {
-                id: user.rows[0].id
-            }
-        })
-
-    } catch (error) {
-        console.log("LOGIN ERROR", error);
-        return res.status(500).json({
-            message: "Internal server error"
-        });
-    }
 }
 
 
