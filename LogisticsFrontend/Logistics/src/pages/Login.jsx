839dbdf159f547d03178204456db515ee759325f
Kelechi <ekelechi413@gmail.com>
2026-06-11 19:42:30 +0100

    done

--- LogisticsFrontend/Logistics/src/pages/Login.jsx
+++ LogisticsFrontend/Logistics/src/pages/Login.jsx
@@ -27,8 +27,7 @@ const Login = () => {
         else {
             navigate("/customer/dashboard");
         }
-         console.log(res.data);console.log("LOGIN RESPONSE:", res.data);
-console.log("ROLE:", res.data.user.role);
+         console.log(res.data);
         } catch (error) {
            console.log("Error in log in", error.response?.data);
     setError(error.response?.data?.message || "Failed to login");
