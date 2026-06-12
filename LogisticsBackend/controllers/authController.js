9557f043b6b253c6ad4b73aa2362f8a0219823fe
Kelechi <ekelechi413@gmail.com>
2026-06-13 00:00:52 +0100

    done

--- LogisticsBackend/controllers/authController.js
+++ LogisticsBackend/controllers/authController.js
@@ -39,12 +39,6 @@ const register = async (req, res) => {
 //LOGIN A USER
 const login = async (req, res) => {
    const { email, password } = req.body;
-
-   if(!form.email.trim() || !form.password.trim()){
-    return res.status(400).json({
-        message: "Your fields are empty"
-    })
-   }
     try {
         const result = await pool.query("SELECT * FROM users  WHERE email = $1", [email]);
         const user = result.rows[0];
